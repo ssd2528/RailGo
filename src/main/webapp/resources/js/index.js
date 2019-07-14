2f0e632e50d40ef0fdea8015bb9f28f680c6909d
sugarS2 <tldnr95@gmail.com>
2019-07-14 14:34:55 +0900

    search-modal 수정

--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -3,23 +3,28 @@ $(document).ready(function(){
 	$('.search-city').on('click', function(){ 
 		$('.search-modal').css('display', 'block'); 
 		$('.searchText').focus();
-		$('body').css('overflow','hidden'); // 모달 외에는 스크롤 막게하기
+		$('html, body').css({'overflow':'hidden', 'height':'100%'});
+		
+		
 	});
 	
 	// search-modal에서 x버튼 클릭시 modal 안보이게 하기
-	$('.closeBtn').on('click', function(){ $('.search-modal').css('display', 'none'); });
+	$('.closeBtn').on('click', function(){ 
+		$('.search-modal').css('display', 'none'); 
+		$('html, body').css({'overflow':'auto', 'height':'100%'});
+	});
 	
 	// search-modal 외 검정바탕 클릭시 modal 안보이게 하기
 	$('html').on('click', function(e){
 		if(e.target.class === 'search-modal') {
 			$('.search-modal').css('display', 'none');
 		}
-	});
-	
+	});	
 	
 	// 지역별 중 하나 클릭 시 하위 도시들 보이게 하기 
 	$('.region-item').bind('click change',function(){
 		$(this).children('.region-city-list').toggleClass("show-city-list");
+		$('.region-item').not(this).toggleClass("show-region-item");
 		$(this).children('.toggle-btn').text(
 				$(this).children('.toggle-btn').text() == '<' ? '>' : '<'
 		);
