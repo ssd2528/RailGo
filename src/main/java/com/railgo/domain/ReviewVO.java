adc459552bc685bae8fe5573a02d900349505fdf
sugarS2 <tldnr95@gmail.com>
2019-07-16 11:32:38 +0900

    VO수정

--- src/main/java/com/railgo/domain/ReviewVO.java
+++ src/main/java/com/railgo/domain/ReviewVO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 
 @Data
 public class ReviewVO {
+	private String r_code; // 리뷰 코드(숙,식,관,공,행,SNS 통합코드)
 	private String code; // 숙,식,관,공,행,SNS 통합코드
 	private int mem_code; // 회원 코드
 	private int rating; // 평점
