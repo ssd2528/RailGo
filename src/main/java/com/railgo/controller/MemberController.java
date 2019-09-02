b65f9834b1b3caf3047eace9649e62b8f163e451
SamSung <SamSung@Lht-Note>
2019-09-02 16:07:33 +0900

    sns 댓글 수정

--- src/main/java/com/railgo/controller/MemberController.java
+++ src/main/java/com/railgo/controller/MemberController.java
@@ -26,6 +26,7 @@ import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,7 +39,9 @@ import com.railgo.domain.FollowingVO;
 import com.google.gson.Gson;
 import com.railgo.domain.MemberAddVO;
 import com.railgo.domain.MemberVO;
+import com.railgo.domain.PlannerJsonDTO;
 import com.railgo.service.MemberService;
+import com.railgo.service.PlannerService;
 
 import lombok.AllArgsConstructor;
 import lombok.Setter;
@@ -53,6 +56,7 @@ public class MemberController {
 	@Setter(onMethod_=@Autowired)
 	MemberService memberService;
 	LoginController loginController;
+	PlannerService plannerService;
 	
 	//타임라인 페이지
 	@GetMapping("/timeline")
